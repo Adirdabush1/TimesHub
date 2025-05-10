d7f5279e2e3b10e101b8c5c13d38f4939fdb0419
Adirdabush1 <adirdabosh33@gmail.com>
2025-05-10 19:27:55 +0300

    Auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Auth from "./pages/Auth";
+// import Auth from "./pages/Auth";
 import News from "./pages/News";
 import { UserProvider } from "./components/UserContext";
 import Payment from "./pages/Payment";
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
+        {/* <Route path="/auth" element={<Auth />} /> */}
         <Route path="/article" element={<News />} />
         <Route path="/Payment" element={<Payment />} />
         <Route path="/TankYou" element={<ThankYou />} />
